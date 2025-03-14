4f295878fd3ebad7463c935b3fde1a4fe8aaf7da
Sledoff <norscreecs@gmail.com>
2025-03-14 16:47:09 +0300

    Version 1

--- main.c
+++ main.c
@@ -1,7 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-
-int main(void)
+char *reading()
 {
-	
+	char *input_string = NULL;
+	size_t len =0;
+	getline(&input_string, &len, stdin);
+	return input_string;
+}
+
+char **parsing_string(char *full_string)
+{
+	char **arg = malloc(64 * sizeof(char*));
+	char *token;
+	int position =0;
+
+	token = strtok(full_string, " \t\n\r");
+	while(token != NULL)
+	{
+		arg[position++] = token;
+		token = strtok(NULL, " \t\n\r");
+	}
+	arg[position] = NULL;
+	return arg;
+}
+
+void exsisting_cmd(char **arg)
+{
+	pid_t pid = fork();
+    if (pid == 0)
+    { 
+        execvp(arg[0], arg);
+        perror("Ошибка запуска");
+        exit(EXIT_FAILURE);
+    } else if (pid > 0) {
+        waitpid(pid, NULL, 0);
+    } else
+        perror("Ошибка fork");
+}
+
+
+int main()
+{
+	char *string;
+	char *arg;
+
+	while(1)
+	{
+		printf("$ ");
+		string = reading();
+		arg = parsing_string(string);
+
+		if (arg[0] != NULL) {
+            exsisting_cmd(arg);
+        }
+
+        free(string);
+        free(arg);
+	}
 	return 0;
 }
